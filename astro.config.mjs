e7ef7c6a29f77120c4343034f053dd06d1dbc645
a-coman <141495654+a-coman@users.noreply.github.com>
2026-03-31 14:51:58 +0200

    Remove tailwind and upgrade packages version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
-  base: '/llm-viewer/',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  base: '/llm-viewer/'
 });
